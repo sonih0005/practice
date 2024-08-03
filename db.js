1e0b1fa1510e05e6628d9ec27a9eb09a7e055d16
sonih0005 <sonih0005@gmail.com>
2024-08-03 19:24:51 +0530

    atlas connection

--- db.js
+++ db.js
@@ -1,10 +1,23 @@
 import mongoose from "mongoose";
 
+// const DB_NAME = 'hotel';
 
-mongoose.connect("mongodb://localhost:27017/hotel", {
+mongoose.connect(`mongodb+srv://sonih0007:harshitsoni123@cluster0.zkxy5nv.mongodb.net/`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+
+// const connectDB = async () => {
+//     try {
+//         const connnectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+//         console.log(`\n MongoDB connected !! DB HOST ${connnectionInstance.Connection.host}`)
+//     } catch (error) {
+//         console.log("MongoDB Connection Error", error);
+//         process.exit(1);
+//     }
+// }
+
+// connectDB();
 
 const db = mongoose.connection;
 
